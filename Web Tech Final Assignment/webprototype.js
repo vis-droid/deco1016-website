9e29c186a16b0c251700eacb56b34abd3f315443
vis-droid <57381182+vis-droid@users.noreply.github.com>
2019-11-11 22:30:47 +1100

    initial commit
    This is the first commit for submission 11th Nov Web Tech. (Specsavers Web Prototype)

---
+++ Web Tech Final Assignment/webprototype.js
@@ -0,0 +1,10 @@
+// this toggle changes the class every time users click on the menu icon
+function onClickMenu(){
+
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+
+
